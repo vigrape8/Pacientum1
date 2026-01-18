1c737f51c730292063fc804e2d6dd1453cc27df5
victor <victor.g.perez11@gmail.com>
2026-01-18 19:51:44 +0100

    --v2-- Se añade el menu a la toolbar

--- app/src/main/java/com/example/pacientum/HomeActivity.kt
+++ app/src/main/java/com/example/pacientum/HomeActivity.kt
@@ -1,20 +1,50 @@
 package com.example.pacientum
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.pacientum.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding= ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        appBarConfiguration= AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+    //estrella
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_enfermero, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(@NonNull item: MenuItem): Boolean {
+        return onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item)
+    }
+    //boton de devolver
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
